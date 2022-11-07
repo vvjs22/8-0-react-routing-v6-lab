ec59821bf84b6a0e3b965cf938281714478c97e7
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-11-06 22:16:03 -0500

    Complete

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,22 +1,23 @@
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <header>
       <article>
         <h1>
-          <a href="/">
+          <Link to="/">
             Northwest <span>Animal Hospital</span>
-          </a>
+          </Link>
         </h1>
       </article>
       <aside>
         <ul>
           <li>
-            <a href="/staff">All Staff</a>
+            <Link to="/staff">All Staff</Link>
           </li>
           <li>
-            <a href="/pets">All Pets</a>
+            <Link to="/pets">All Pets</Link>
           </li>
         </ul>
       </aside>
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,15 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { useParams, Navigate } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
+  let { kind } = useParams();
+
+  if (!kind) {
+    return <Navigate to="/pets/cats" />;
+  }
+
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -17,14 +24,14 @@ export const PetsList = ({ pets }) => {
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        {kind === "cats"
+          ? cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)
+          : null}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {kind === "dogs"
+          ? dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)
+          : null}
       </section>
     </section>
   );
